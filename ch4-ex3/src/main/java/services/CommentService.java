0e75ffabbef79583dd543c7d99088a75b5ee23a2
Thomas-Jiao <thomasjiao@outlook.com>
2024-11-18 13:56:41 +0100

    Complete ch4-ex3

---
+++ ch4-ex3/src/main/java/services/CommentService.java
@@ -0,0 +1,24 @@
+package services;
+
+import repositories.CommentRepository;
+import proxies.CommentNotificationProxy;
+import model.Comment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
+    
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+}
