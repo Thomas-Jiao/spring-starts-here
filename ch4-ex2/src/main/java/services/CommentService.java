e4ca949e5cd2c7eebd7f6e4973771a2ad025f818
Thomas-Jiao <thomasjiao@outlook.com>
2024-11-18 11:55:39 +0100

    Complete ch4-ex2

---
+++ ch4-ex2/src/main/java/services/CommentService.java
@@ -0,0 +1,27 @@
+package services;
+
+import repositories.CommentRepository;
+import proxies.CommentNotificationProxy;
+import model.Comment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, 
+                          CommentNotificationProxy commentNotificationProxy) {
+
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+}
