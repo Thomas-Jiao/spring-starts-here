593d206e71a591c0196427905774702f278ff8bd
Thomas JIAO <thomasjiao@outlook.com>
2024-12-03 15:41:40 +0100

    Test Spring's behavior for our singleton bean

--- ch5-ex1/src/main/java/main/Main.java
+++ ch5-ex1/src/main/java/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import configuration.ProjectConfig;
+import services.CommentService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var cs1 = c.getBean("commentService", CommentService.class);
+        var cs2 = c.getBean("commentService", CommentService.class);
+
+        boolean b1 = cs1 == cs2;
+
+        System.out.println(b1);
     }
 }
